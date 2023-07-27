8a48a1d2f1c796cf7e825a7594f47c9c319e074c
Matheus Bispo <matheus.bispo@airapps.com>
2023-07-27 14:25:54 -0300

    feat: update dependencies URLs for iOS 11 compatibility

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.5"),
+    .package(url: "https://github.com/matheus-air/swift-concurrency-extras", branch: "ios-11-compatibility"),
+    .package(url: "https://github.com/matheus-air/xctest-dynamic-overlay", branch: "ios-11-compatibility"),
   ],
   targets: [
     .target(
